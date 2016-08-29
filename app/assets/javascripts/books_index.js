dc44d2fd86e32d0b1d00235c6ad9cfe3deccae65
MinhDuc <duc11t3bk@gmail.com>
2016-08-29 12:50:34 +0700

    74591_filter_book

--- app/assets/javascripts/books_index.js
+++ app/assets/javascripts/books_index.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
+$(document).on("ready page:load", function(){
   $(".star-rating").rating({
     displayOnly: true,
   });
 });
+function changeDataFilter(checkbox){
+  $(checkbox).closest("form").submit();
+}
--- app/models/book.rb
+++ app/models/book.rb
@@ -12,4 +12,19 @@ class Book < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 10, maximum: 100}
   validates :description, presence: true, length: {minimum: 10}
   validates :publish_date, presence: true
+
+  default_scope {order created_at: :desc}
+
+  scope :of_category, -> category_id do
+    where category_id: category_id if category_id.present?
+  end
+  scope :of_author, -> author_id do
+    where author_id: author_id if author_id.present?
+  end
+  scope :of_publisher, -> publisher_id do
+    where publisher_id: publisher_id if publisher_id.present?
+  end
+  scope :of_language, -> language_id do
+    where language_id: language_id if language_id.present?
+  end
 end
