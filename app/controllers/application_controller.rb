34295a60d8fb753c8ea6cfaa464bc29a9b9b43f8
Duc Filan <ducfilan@gmail.com>
2016-08-29 10:26:11 +0700

    Create static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
   def configure_permitted_parameters
@@ -12,5 +13,10 @@ class ApplicationController < ActionController::Base
       user_params.permit :name, :email, :password,
         :password_confirmation, :current_password
     end
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
